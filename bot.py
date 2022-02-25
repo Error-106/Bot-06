1cb464d58a9f5abc0ba5d9ff304063b275bf58e4
Error-106 <92155028+Error-106@users.noreply.github.com>
2022-02-25 19:21:45 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,6 +11,7 @@ import re
 import io
 #import os
 from PIL import Image
+from sqlalchemy import true
 def textcolor(code='0'):
         return '\033[%sm'%code
 log=None
@@ -35,10 +36,14 @@ class bot:
     def keyword(Type,message,allmssage,uid,gid=None):
         #Type:消息类型（私聊：1，群聊：2，频道：3），message：消息，uid:QQ号，gid：群号/频道号
         if log == None: bot.loadlog()
-        if Type=="2" and gid== "1013983357":
+        if Type=="2":
             scans=bot.nsfwscan(message=message)
+            scansflag=False
             for scan in scans:
-                print(bot.nsfwif(scans[scan]))
+                if bot.nsfwif(scans[scan]):
+                    scansflag=True
+            if scansflag:
+                pass
             print(scans)
 #        if Type==3 and uid!=log[1]["tiny_id"] and gid==["2307805s1636521595","1780294"]:
 #            url='http://127.0.0.1:5700/send_guild_channel_msg?guild_id='+gid[0]+"&channel_id="+gid[1]+"&message="+message
@@ -48,6 +53,7 @@ class bot:
         reimgs={}
         if imgs != []:
             for i in imgs:
+                print(i)
                 img=Image.open(io.BytesIO(requests.get(i[1]).content))
                 insfw=nsfw.get_tasks(img)
                 reimgs[i]=insfw
